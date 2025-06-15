1b97051ac808dbff04260e646d4eca61a4197c89
Jung Hae Kyoung <jennyjung.dev@gmail.com>
2025-06-15 22:24:35 +0900

    Feat: Shandcn Table 추가

--- src/app/shadcn/page.tsx
+++ src/app/shadcn/page.tsx
@@ -9,32 +9,76 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { selectList } from "@/lib/data";
+import { invoices, selectList } from "@/lib/data";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 export default function ShadcnPage() {
   return (
     <div className="flex flex-col gap-4 py-4">
-      <div className="flex gap-2">
-        <Input type="email" placeholder="Email" className="w-60" />
-        <Button>Button</Button>
+      <div className="flex justify-between">
+        <div className="flex gap-2">
+          <Input type="email" placeholder="Email" className="w-60" />
+          <Button>Button</Button>
+        </div>
+        <div className="flex gap-2">
+          <Select>
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="Select a fruit" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>Fruits</SelectLabel>
+                {selectList.map(({ value, label }) => (
+                  <SelectItem value={value} key={value}>
+                    {label}
+                  </SelectItem>
+                ))}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+          <Button variant="outline">Button</Button>
+        </div>
       </div>
-      <div className="flex gap-2">
-        <Select>
-          <SelectTrigger className="w-[180px]">
-            <SelectValue placeholder="Select a fruit" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              <SelectLabel>Fruits</SelectLabel>
-              {selectList.map(({ value, label }) => (
-                <SelectItem value={value} key={value}>
-                  {label}
-                </SelectItem>
-              ))}
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-        <Button variant="outline">Button</Button>
+
+      <div>
+        <Table>
+          <TableCaption>A list of your recent invoices.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">Invoice</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Method</TableHead>
+              <TableHead className="text-right">Amount</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {invoices.map((invoice) => (
+              <TableRow key={invoice.invoice}>
+                <TableCell className="font-medium">{invoice.invoice}</TableCell>
+                <TableCell>{invoice.paymentStatus}</TableCell>
+                <TableCell>{invoice.paymentMethod}</TableCell>
+                <TableCell className="text-right">
+                  {invoice.totalAmount}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TableCell colSpan={3}>Total</TableCell>
+              <TableCell className="text-right">$2,500.00</TableCell>
+            </TableRow>
+          </TableFooter>
+        </Table>
       </div>
     </div>
   );
